4fa703fa159e8c9f412f50f44cca8c3092805cad
Marin Procureur <marin.procureur@gmail.com>
2019-01-27 00:32:53 +0100

    add dist folder to push (use gh pages for now)

---
+++ dist/js/about.86cb28bb.js
@@ -0,0 +1,8 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([['about'], {
+  f820(t, e, n) {
+    'use strict';
+
+    n.r(e); const a = function () { const t = this; const e = t.$createElement; t._self._c; return t._m(0); }; const s = [function () { const t = this; const e = t.$createElement; const n = t._self._c || e; return n('div', { staticClass: 'about' }, [n('h1', [t._v('This is an about page')])]); }]; const u = n('2877'); const i = {}; const o = Object(u.a)(i, a, s, !1, null, null, null); o.options.__file = 'About.vue'; e.default = o.exports;
+  },
+}]);
+// # sourceMappingURL=about.86cb28bb.js.map
---
+++ dist/precache-manifest.4c944abac1fd163155ef1aadf1759cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/robots.txt',
+  },
+  {
+    revision: '873fbcde791667db26ee',
+    url: '/js/chunk-vendors.57e71dbf.js',
+  },
+  {
+    revision: '9acb456a9883a54243b0',
+    url: '/js/app.852488ca.js',
+  },
+  {
+    revision: '619b938c732988046d50',
+    url: '/js/about.86cb28bb.js',
+  },
+  {
+    revision: '47f811305f77ea3c81098ed06fda3b24',
+    url: '/index.html',
+  },
+  {
+    revision: '9acb456a9883a54243b0',
+    url: '/css/app.8565ede6.css',
+  },
+];
